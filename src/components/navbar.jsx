6978852097ab8642f01ceb2460f58b842170b97a
YashvardhanKumar <imt_2020118@iiitm.ac.in>
2022-01-25 02:11:04 +0530

    Added Basic Navbar and search bar components

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import './navbar.css'
+import SearchBox from './searchBox'
+
+const Navbar = () => {
+    return (<nav>
+        <h2>Good Merchant</h2>
+        <SearchBox/>
+    </nav>)
+}
+
+export default Navbar
---
+++ src/components/searchBox.jsx
@@ -0,0 +1,11 @@
+import './searchBox.css'
+
+const SearchBox = () => {
+    return (<input
+    placeholder="Search"
+    name="q"
+    type="text"
+    />)
+}
+
+export default SearchBox
