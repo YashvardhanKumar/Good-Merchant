e97278a2f2ce37f11bee32136b8f67c7be72ec3f
YashvardhanKumar <imt_2020118@iiitm.ac.in>
2022-01-25 10:49:00 +0530

    resolved a bug

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
-import logo from '../logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
-import Navbar from '../navbar';
+import Navbar from '../navbar/navbar';
 
 function App() {
   return (
