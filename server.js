769df8357b70ee950a5b8b0ffc988c2a6d36ee16
YashvardhanKumar <imt_2020118@iiitm.ac.in>
2022-02-21 17:29:45 +0530

    express-rate-limit added

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const path = require('path');
 const axios = require('axios');
 // Loads env variables
 require('dotenv').config()
+import rateLimit from 'express-rate-limit'
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 // Adds json parsing middleware
